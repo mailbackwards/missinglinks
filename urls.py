46e1dde56ad7f23f61cfa6f96cffe026314b7b2d
Liam Andrew <landrew@texastribune.org>
2015-10-25 22:57:06 -0500

    Add favicon to urls

--- urls.py
+++ urls.py
@@ -2,7 +2,8 @@ from django.conf.urls import include, url
 from django.conf import settings
 from django.contrib import admin
 
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, RedirectView
+from django.contrib.staticfiles.templatetags.staticfiles import static
 
 from wagtail.wagtailadmin import urls as wagtailadmin_urls
 from wagtail.wagtaildocs import urls as wagtaildocs_urls
@@ -17,17 +18,17 @@ urlpatterns = [
 
     url(r'^search/$', 'search.views.search', name='search'),
 
-    url(r'', include(wagtail_urls)),
-
     url(r'^robots\.txt$', TemplateView.as_view(template_name='robots.txt',
-                                               content_type='text/plain'))
+                                               content_type='text/plain')),
+    url(r'^favicon\.ico$', RedirectView.as_view(url=static('img/favicon.ico'))),
+
+    url(r'', include(wagtail_urls)),
 ]
 
 
 if settings.DEBUG:
     from django.conf.urls.static import static
     from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-    from django.views.generic import TemplateView
 
     # Serve static and media files from development server
     urlpatterns += staticfiles_urlpatterns()
