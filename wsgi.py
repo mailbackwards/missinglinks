0cfd584b343dafd9a36ce7a08388e4f078a27777
Liam Andrew <landrew@texastribune.org>
2015-10-25 22:56:52 -0500

    Setup static and media assets

--- wsgi.py
+++ wsgi.py
@@ -7,9 +7,9 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 import environ
-
 environ.Env().read_env()
 
 from django.core.wsgi import get_wsgi_application
+from dj_static import Cling, MediaCling
 
-application = get_wsgi_application()
+application = Cling(MediaCling(get_wsgi_application()))
